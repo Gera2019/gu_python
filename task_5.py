6602a9e2ec375e0b701998aff2de9b0ce9aa0d78
Gera2019 <gumanoid2@gmail.com>
2021-02-10 22:18:41 +0300

    updated files task_1.py, task_2_3.py, task_4.py, task_5.py

--- task_5.py
+++ task_5.py
@@ -1,11 +1,19 @@
 goods = [78.8, 89.9, 150, 134.50, 678.99, 39.99, 20.89, 77.69, 145.90, 6.90, 14.44, 156, 67.9]
-
+new_list = []
 for item in goods:
+    rub = f'{int(item):02d} руб'
+    kop = f'{round((item % int(item) * 100)):02d} коп'
+    new_list.append(f'{rub} {kop}')
 
-    print(f'{int(item):02d} руб {round((item % int(item)*100)):02d} коп', sep=",")
+print(', '.join(new_list))
 
+# Вывести цены, отсортированные по возрастанию, новый список не создавать
+# (доказать, что объект списка после сортировки остался тот же)
 print(sorted(goods))
 print(goods)
-
+# Создать новый список, содержащий те же цены, но отсортированные по убыванию.
 goods_sorted = sorted(goods).copy()
-print(sorted(goods_sorted, 1, reversed()))
+print(list(reversed(goods_sorted)))
+
+# Вывести цены пяти самых дорогих товаров.
+print(sorted(goods)[-5:])
