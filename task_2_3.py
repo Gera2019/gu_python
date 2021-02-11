654d1f06caf0b0226221a9fddcf7a36613d0bca1
Gera2019 <gumanoid2@gmail.com>
2021-02-11 13:24:54 +0300

    updated file task_2_3.py

--- task_2_3.py
+++ task_2_3.py
@@ -1,7 +1,11 @@
 info = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
-for item in info:
-    if item.isdigit():
-        print(f'"{int(item):02d}"', end=' ')
-    else:
-        print(f'{item}', end=' ')
+for idx in range(len(info)):
+    if info[idx].isdigit():
+        info[idx] = ''.join(['"', f'{int(info[idx]):02d}', '"'])
+
+    elif ('+' in info[idx] or '-' in info[idx]) and info[idx][1].isdigit():
+        info[idx] = ''.join(['"', f'{info[idx][0]}{int(info[idx][1:]):02d}', '"'])
+
+print(' '.join(info))
+
