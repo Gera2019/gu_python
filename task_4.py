a2b92b616af974fcb478b96cd12447381a147c2f
Gera2019 <gumanoid2@gmail.com>
2021-02-12 10:45:11 +0300

    updated task_2_3.py and task_4.py

--- task_4.py
+++ task_4.py
@@ -1,6 +1,5 @@
 employers = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор '
                                                                                                         'аэлита']
-
 for info in employers:
     person = info.split(" ")
     print(f'Привет, {person[-1].capitalize()}!')
