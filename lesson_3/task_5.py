00a2fc1c478792c164be5e856a59e3b3b53e86bd
Gera2019 <gumanoid2@gmail.com>
2021-02-15 12:44:41 +0300

    added task_5.py

--- lesson_3/task_5.py
+++ lesson_3/task_5.py
@@ -4,12 +4,13 @@ nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
+words_array = [nouns, adverbs, adjectives]
 
 def get_jokes(n):
     joke = []
     i = 1
     while i <= n:
-        joke.append(' '.join(map(lambda word: choice(word), [nouns, adverbs, adjectives])))
+        joke.append(' '.join(map(lambda word: choice(word), words_array)))
         i += 1
     print(joke)
 
