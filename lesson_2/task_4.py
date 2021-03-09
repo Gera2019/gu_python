8157146a46046e8f349c77dab940e5f8585b5a59
Gera2019 <47034214+Gera2019@users.noreply.github.com>
2021-03-09 06:21:15 +0300

    Delete task_4.py

--- lesson_2/task_4.py
+++
@@ -1,5 +0,0 @@
-employers = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор '
-                                                                                                        'аэлита']
-for info in employers:
-    person = info.split(" ")
-    print(f'Привет, {person[-1].capitalize()}!')
